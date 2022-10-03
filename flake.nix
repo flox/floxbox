417f5a06b0f7db61ef6d35cca36b649e29c8d996
Sam Rose <sam@floxdev.com>
2022-10-03 09:44:10 -0400

    packer

--- flake.nix
+++ flake.nix
@@ -4,66 +4,65 @@
   # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "github:flox/nixpkgs/unstable";
 
-  outputs = { self, nixpkgs }:
-    let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # to work with older version of flakes
+    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
 
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+    # Generate a user-friendly version number.
+    version = builtins.substring 0 8 lastModifiedDate;
 
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 lastModifiedDate;
+    # System types to support.
+    supportedSystems = ["x86_64-linux"];
 
-      # System types to support.
-      supportedSystems = [ "x86_64-linux"];
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    # Provide some binary packages for selected system types.
+    packages = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      itest = pkgs.buildGo118Module {
+        pname = "itest";
+        inherit version;
+        # In 'nix develop', we don't need a copy of the source tree
+        # in the Nix store.
+        src = ./.;
+        buildInputs = [pkgs.qemu pkgs.packer];
+        # This hash locks the dependencies of this package. It is
+        # necessary because of how Go requires network access to resolve
+        # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+        # details. Normally one can build with a fake sha256 and rely on native Go
+        # mechanisms to tell you what the hash should be or determine what
+        # it should be "out-of-band" with other tooling (eg. gomod2nix).
+        # To begin with it is recommended to set this, but one must
+        # remeber to bump this hash when your dependencies change.
+        vendorSha256 = null;
 
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+        #vendorSha256 = "sha256-/n9+acNntoOpzNwWT2ow77SLzRJMGnwyiqzh2t/48gY=";
+      };
+    });
 
+    # The default package for 'nix build'. This makes sense if the
+    # flake provides only one package or there is a clear "main"
+    # package.
+    defaultPackage = forAllSystems (system: self.packages.${system}.itest);
+    devShell = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
     in
-    {
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          itest = pkgs.buildGo118Module {
-            pname = "itest";
-            inherit version;
-            # In 'nix develop', we don't need a copy of the source tree
-            # in the Nix store.
-            src = ./.;
-	    buildInputs = [ pkgs.qemu ];
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake sha256 and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remeber to bump this hash when your dependencies change.
-            vendorSha256 = null;
-
-            #vendorSha256 = "sha256-/n9+acNntoOpzNwWT2ow77SLzRJMGnwyiqzh2t/48gY=";
-          };
-        });
-
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.itest);
-      devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.packer
-          ];
-          shellHook = ''
-            export HELLO="hello"
-          '';
-        };
-      hydraJobs = forAllSystems (system: self.packages.${system}.itest);
-    };
+      pkgs.mkShell {
+        buildInputs = [
+          pkgs.packer
+        ];
+        shellHook = ''
+          export HELLO="hello"
+        '';
+      });
+    hydraJobs = forAllSystems (system: self.packages.${system}.itest);
+  };
 }
